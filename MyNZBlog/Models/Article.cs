e2bf36c05ac58917813181cad46cd9f8cd66be72
nphumy98 <38494628+nphumy98@users.noreply.github.com>
2020-04-06 00:37:05 +1200

    fix extra paging

--- MyNZBlog/Models/Article.cs
+++ MyNZBlog/Models/Article.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
  using System.ComponentModel.DataAnnotations;
  using System.Linq;
 using System.Threading.Tasks;
+ using Microsoft.AspNetCore.Mvc.Formatters;
 
-namespace MyNZBlog.Models
+ namespace MyNZBlog.Models
 {
     public class Article
     {
         public Article()
         {
             ArticleHasTags = new List<ArticleHasTag>();
+            ReleaseDate = DateTime.Now;
         }
         public int Id { get; set; }
         public string Title { get; set; }
