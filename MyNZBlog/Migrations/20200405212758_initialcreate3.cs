dc986166c3f1bb1c7fc2d99bf5f82089927bcee1
nphumy98 <38494628+nphumy98@users.noreply.github.com>
2020-04-06 11:49:47 +1200

    hdfghf

---
+++ MyNZBlog/Migrations/20200405212758_initialcreate3.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MyNZBlog.Migrations
+{
+    public partial class initialcreate3 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
